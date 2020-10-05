13077875a966eb4d32ffa16667fc5dca434e9c3d
Alexander Makarov <samdark@users.noreply.github.com>
2020-10-05 19:06:32 +0000

    Apply fixes from StyleCI

--- config/common.php
+++ config/common.php
@@ -12,7 +12,7 @@ use Yiisoft\Log\Target\File\FileTarget;
 /* @var $params array */
 
 return [
-    LoggerInterface::class => static fn(FileTarget $fileTarget) => new Logger(['file' => $fileTarget]),
+    LoggerInterface::class => static fn (FileTarget $fileTarget) => new Logger(['file' => $fileTarget]),
     FileRotatorInterface::class => function () use ($params) {
         return new FileRotator(
             $params['yiisoft/log-target-file']['file-rotator']['maxFileSize'],
