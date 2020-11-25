465b18a25965d72891e2626e7e1c019bec82e2ee
devanych <mail@devanych.ru>
2020-11-25 12:12:26 +0300

    Add parent constructor call to FileTarget

--- src/FileTarget.php
+++ src/FileTarget.php
@@ -50,6 +50,7 @@ class FileTarget extends Target
         $this->rotator = $rotator;
         $this->dirMode = $dirMode;
         $this->fileMode = $fileMode;
+        parent::__construct();
     }
 
     /**
