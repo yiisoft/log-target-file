e64763e417cc55202ddac084863686aad078f940
Rustam <rmamdaminov@gmail.com>
2022-07-24 01:28:07 +0500

    Add config test

---
+++ tests/ConfigTest.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Log\Target\File\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Di\Container;
+use Yiisoft\Di\ContainerConfig;
+use Yiisoft\Log\Target\File\FileRotator;
+use Yiisoft\Log\Target\File\FileRotatorInterface;
+use Yiisoft\Log\Target\File\FileTarget;
+
+final class ConfigTest extends TestCase
+{
+    public function testBase(): void
+    {
+        $container = $this->createContainer();
+
+        $fileRotator = $container->get(FileRotatorInterface::class);
+        $fileTarget = $container->get(FileTarget::class);
+
+        $this->assertInstanceOf(FileRotator::class, $fileRotator);
+        $this->assertInstanceOf(FileTarget::class, $fileTarget);
+    }
+
+    private function createContainer(?array $params = null): Container
+    {
+        return new Container(
+            ContainerConfig::create()->withDefinitions(
+                $this->getCommonDefinitions($params)
+            )
+        );
+    }
+
+    private function getCommonDefinitions(?array $params = null): array
+    {
+        if ($params === null) {
+            $params = $this->getParams();
+        }
+        return require dirname(__DIR__) . '/config/common.php';
+    }
+
+    private function getParams(): array
+    {
+        return require dirname(__DIR__) . '/config/params.php';
+    }
+}
